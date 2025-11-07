46a7076ed4f7441926bffc3372b0472ca6b88b0c
IK240100\IK240100 <harisrsyd@gmail.com>
2025-11-07 10:19:56 +0700

    style(font): add google font (Ubuntu, Inter, Keania One, Momo Signature) and change logo font use Keania One

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,11 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Keania+One&family=Momo+Signature&family=Saira:ital,wght@0,100..900;1,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+    rel="stylesheet">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
